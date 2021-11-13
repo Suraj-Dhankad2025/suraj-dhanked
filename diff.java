22862db2ce865fd1cab7871cbb27f3b97cc68b74
suraj dhanked <suraj.dhanked@gmail.com>
2021-11-13 18:23:04 +0530

    added diff.java git commit -m added

---
+++ diff.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class diff {
+  public static void main(String args[]){
+        System.out.println("enter first number:");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("enter second number");
+        int b = sc.nextInt();
+        int diff = a - b;
+        System.out.println("the sum of two numbers is" + " " + diff);
+        System.out.println("this is your answer");
+    
+}
+}
